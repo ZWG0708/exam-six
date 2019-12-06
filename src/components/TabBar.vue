6db0f849e365f800e999f516b92f16def1104d97
ZWG0708 <1298478365@qq.com>
2019-12-06 09:12:58 +0800

    exam-six

---
+++ src/components/TabBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="tab-bar">
+        <router-link to="/home">首页</router-link>
+        <router-link to="/about">我的</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.tab-bar{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    line-height: 50px;
+    border-top: 1px solid #ccc;
+    display: flex;
+    a{
+        flex: 1;
+        text-align: center;
+        text-decoration: none;
+        &.router-link-active{
+            color: greenyellow;
+        }
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="home">
+    <div class="main">
+      <van-swipe :autoplay="2000" indicator-color="lime">
+        <van-swipe-item>
+          <img src="//dummyimage.com/100x100" alt />
+        </van-swipe-item>
+        <van-swipe-item>
+          <img src="//dummyimage.com/100x100" alt />
+        </van-swipe-item>
+        <van-swipe-item>
+          <img src="//dummyimage.com/100x100" alt />
+        </van-swipe-item>
+        <van-swipe-item>
+          <img src="//dummyimage.com/100x100" alt />
+        </van-swipe-item>
+        <van-swipe-item>
+          <img src="//dummyimage.com/100x100" alt />
+        </van-swipe-item>
+      </van-swipe>
+      <div class="navList">
+        <dl v-for="item in list" :key="item.id">
+          <dt>
+            <img :src="item.img" alt />
+          </dt>
+          <dd>{{item.title}}</dd>
+        </dl>
+      </div>
+      <div class="content">
+          <img src="//dummyimage.com/200x100" alt="" v-for="n in 4" :key="n">
+      </div>
+    </div>
+    <TabBar></TabBar>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Swipe, SwipeItem } from "vant";
+Vue.use(Swipe).use(SwipeItem);
+export default {
+  name: "home",
+  data() {
+    return {
+      list: [
+        {
+          id: 0,
+          img: "//dummyimage.com/50x50",
+          title: "绩效考核"
+        },
+        {
+          id: 1,
+          img: "//dummyimage.com/50x50",
+          title: "绩效考核"
+        },
+        {
+          id: 2,
+          img: "//dummyimage.com/50x50",
+          title: "绩效考核"
+        },
+        {
+          id: 3,
+          img: "//dummyimage.com/50x50",
+          title: "绩效考核"
+        },
+        {
+          id: 4,
+          img: "//dummyimage.com/50x50",
+          title: "绩效考核"
+        },
+        {
+          id: 5,
+          img: "//dummyimage.com/50x50",
+          title: "绩效考核"
+        },
+        {
+          id: 6,
+          img: "//dummyimage.com/50x50",
+          title: "绩效考核"
+        },
+        {
+          id: 7,
+          img: "//dummyimage.com/50x50",
+          title: "绩效考核"
+        }
+      ]
+    };
+  },
+  components: {}
+};
+</script>
+
+<style lang="scss">
+.content{
+  width: 100%;display: flex;
+  overflow-x: scroll;
+  img{
+    flex-shrink: 0;
+    margin: 0 10px;
+  }
+}
+.main {
+  width: 100%;
+  height: 617px;
+  overflow: auto;
+}
+.van-swipe {
+  width: 100%;
+  height: 200px;
+  margin-top: 20px;
+  img {
+    width: 100%;
+  }
+}
+.navList {
+  width: 100%;
+  height: 200px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 20px;
+  dl {
+    width: 25%;
+    text-align: center;
+    img {
+      border-radius: 50%;
+    }
+  }
+}
+</style>
